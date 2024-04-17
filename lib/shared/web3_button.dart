276f9570fe183b1bf78c8c4a2a2b54d90065e4ac
q4 <dialogues@q4s-MacBook-Pro.local>
2024-04-17 12:56:37 -0700

    + Web3 Elevated Button template

---
+++ lib/shared/web3_button.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class Web3ElevatedButton extends StatefulWidget {
+  const Web3ElevatedButton({super.key});
+
+  @override
+  State<Web3ElevatedButton> createState() => _Web3ElevatedButtonState();
+}
+
+class _Web3ElevatedButtonState extends State<Web3ElevatedButton> {
+  @override
+  Widget build(BuildContext context) {
+    // design elements
+    // TODO shrink height of container by ~3 pxls on hover
+    // anchor the shrink behavior to topcenter
+    return const Placeholder();
+  }
+}
